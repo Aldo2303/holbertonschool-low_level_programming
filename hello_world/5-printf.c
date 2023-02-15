d17ecc409fea7fb773c870739366f17dac0cf58c
Aldo2303 <aldod03@gmail.com>
2023-02-15 14:00:03 -0800

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char print[]=("with proper grammar, but the outcome isia piece of art,");
-	printf("%s\n", print);
+	char *str = "with proper grammar, but the outcome isia piece of art,";
+	 printf("%s\n", str);
 	return (0);
 }
