1882a5dc82fdea47999c766571e3bf7d081f861c
Aldo2303 <aldod03@gmail.com>
2023-02-16 15:12:29 -0800

    basehexa

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char NUMx;
+for (NUMx = '0'; NUMx <= 'f'; NUMx++)
+{
+putchar(NUMx);
+}
+putchar('\n');
+return (0);
+}
