9c590f59270a3305adf7944fe430145d15d46c10
Aldo2303 <aldod03@gmail.com>
2023-02-15 18:37:34 -0800

    $zu

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	char charType;
-	int intType;
-	long int longintType;
-	long long int longlongintType;
-	float floatType;
-	 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-	 printf("Size of an int: %zu bytes(s)\n", sizeof(intType));
-	 printf("Size of a long int: %zu bytes(s)\n", sizeof(longintType));
-	 printf("Size of a long long int: %zu bytes(s)\n", sizeof(longlongintType));
-	 printf("Size of a float: %zu bytes(s)\n", sizeof(floatType));
+char charType;
+int intType;
+long int longintType;
+long long int longlongintType;
+float floatType;
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu bytes(s)\n", sizeof(intType));
+printf("Size of a long int: %zu bytes(s)\n", sizeof(longintegType));
+printf("Size of a long long int: %zu bytes(s)\n", sizeof(longlongintegerType));
+printf("Size of a float: %zu bytes(s)\n", sizeof(floatType));
 return (0);
 }
 
