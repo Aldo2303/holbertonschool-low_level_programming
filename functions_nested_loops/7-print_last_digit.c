fb09d6504a9cc75fff8e03c9874e2107c9e89d0d
Aldo2303 <aldod03@gmail.com>
2023-02-18 11:52:05 -0800

    lastdigit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit -  print the last digit
+ *
+ * Return: Always ld
+ *
+ * @num: variable
+ *
+ */
+int print_last_digit(int num)
+{
+int ld;
+ld = (num % 10);
+if (num < 0)
+{
+ld = (-num % 10);
+}
+_putchar('0' + ld);
+return (ld);
+}
