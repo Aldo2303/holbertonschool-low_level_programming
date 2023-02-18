1846e2fd32ee2e723b18d89a307915bfc7557f95
Aldo2303 <aldod03@gmail.com>
2023-02-17 16:53:07 -0800

    ififelse+0-

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,16 +2,16 @@
 /**
  *print_sign - check >0 print +, if <0 print -, =0 print 0
  *Return: 1 if es +, 0 if is 0 and -1 if is negative
- *@c:(int n)
+ *@n: int n
  */
 int print_sign(int n)
 {
-if (n > '0')
+if (n > 0)
 {
 _putchar('+');
 return (1);
 }
-if (n == '0')
+if (n == 0)
 {
 _putchar('0');
 return (0);
