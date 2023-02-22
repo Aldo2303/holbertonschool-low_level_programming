b1bc39bb04c4e853343a1f3adae5ffba9c560a2c
Aldo2303 <aldod03@gmail.com>
2023-02-22 12:23:35 -0800

    morefun0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper -  check uppercase character
+ *
+ * Return: 1 y 0
+ *
+ * @c: variable
+ *
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
