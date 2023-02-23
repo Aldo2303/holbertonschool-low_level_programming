3ab08393f5e4485d3c6f1993aa0c1d79de1786e6
Aldo2303 <aldod03@gmail.com>
2023-02-23 08:04:02 -0800

    morefun8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -7,13 +7,13 @@
 void print_square(int size)
 {
 int a, b;
-for (b = 0; b < size; b++)
-{
 if (size <= 0)
 {
 _putchar('\n');
 }
 else
+{
+for (b = 0; b < size; b++)
 {
 for (a = 1; a <= size; a++)
 {
