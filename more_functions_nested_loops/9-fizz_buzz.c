e6098c4fc10c4cb860f674751062b2082552e2b6
Aldo2303 <aldod03@gmail.com>
2023-02-23 09:34:28 -0800

    morefun9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - FizzBuzz
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int mul3, b, mul5, mul15;
+
+	for (b = 1; b <= 100; b++)
+{
+	mul3 = b % 3;
+	mul5 = b % 5;
+	mul15 = b % 15;
+	if (mul15 == 0)
+	{
+		printf("FizzBuzz ");
+	}
+	else if (mul5 == 0 && b != 100)
+	{
+		printf("Buzz ");
+	}
+	else if (b == 100)
+	{
+		printf("Buzz");
+	}
+	else if (mul3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else
+	{
+		printf("%d ", b);
+}
+}
+return (0);
+}
