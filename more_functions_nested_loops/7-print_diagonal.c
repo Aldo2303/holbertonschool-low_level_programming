cd3a09de5286028b75caddd87620787caa58ccba
Aldo2303 <aldod03@gmail.com>
2023-02-23 05:37:02 -0800

    morefun7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ *
+ * @n: variable
+ */
+void print_diagonal(int n)
+{
+int a, b;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (a = 1; a <= n; a++)
+{
+if (a > 1)
+{
+for (b = 1; b <= a - 1; b++)
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+}
