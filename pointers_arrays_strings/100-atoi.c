22b237560fde7cefec343f4e62eab5589a0addc6
Aldo2303 <aldod03@gmail.com>
2023-03-01 05:17:19 -0800

    point10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _atoi - function that convert a string to an integer
+ *
+ * @s: variable
+ *
+ * return: int
+ */
+int _atoi(char *s)
+{
+	int x = 0;
+
+	while (*s >= '0' && *s <= '9')
+	{
+		x = (x * 10) + (*s - '0');
+		x++;
+	}
+	return (x);
+}
+
