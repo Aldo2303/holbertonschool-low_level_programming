02d8030c2f7cc07ae4650f5f1b4307acf1e8fd9d
Aldo2303 <aldod03@gmail.com>
2023-03-02 09:15:15 -0800

    point2.3

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * *_strchr - function that locates a character in a string
+ *
+ * @s: variable
+ *
+ * @c: variable
+ *
+ * Return: char
+ */
+char *_strchr(char *s, char c)
+{
+	int x = 0, y;
+	char *p;
+
+	while (s[x])
+	{
+		x++;
+	}
+	for (y = 0; y <= x; y++)
+	{
+		if (s[y] == c)
+		{
+			p = &s[y];
+		}
+		else
+		{
+			return ('\0');
+		}
+	}
+	return (p);
+}
