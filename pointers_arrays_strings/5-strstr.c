c784300e365960f0bf9b2a871e08d77b367e7b18
Aldo2303 <aldod03@gmail.com>
2023-03-02 17:59:19 -0800

    point5.3

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strstr - function that locates a substring
+ *
+ * @haystack: variable
+ *
+ * @needle: variable
+ *
+ * Return: char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *p;
+
+	while (*haystack != '\0' && *needle != '\0')
+	{
+		if (*needle == *haystack)
+		{
+			p = haystack;
+			return (p);
+		}
+		needle++;
+		haystack++;
+	}
+	return ('\0');
+}
+
