c59165c4582f8a3d7d9b29f44ea5a5512311c30c
Aldo2303 <aldod03@gmail.com>
2023-03-03 06:15:16 -0800

    point3.3.

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,19 +10,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int x = 0, y = 0, count = 0;
+	int x, y, count = 0;
 
-	while (accept[y])
+	x = 0;
+	while (s[x] != '\0')
 	{
-		while (s[x] != ' ')
+		y = 0;
+		while (accept[y] != '\0')
 		{
 		if (accept[y] == s[x])
 		{
 			count++;
 		}
-		x++;
-		}
 		y++;
+		}
+		x++;
 		}
 	return (count);
 }
