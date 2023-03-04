54515234ec1181a0ca988e19dffc6852f66e3a58
Aldo2303 <aldod03@gmail.com>
2023-03-03 18:22:48 -0800

    point10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,13 +8,23 @@
  */
 int _atoi(char *s)
 {
+	unsigned int result = 0;
+	unsigned int sign = 1;
 	int x = 0;
 
-	while (*s >= '0' && *s <= '9')
+	if (s[x] == 45)
 	{
-		x = (x * 10) + (*s - '0');
+		sign = -1;
 		x++;
 	}
-	return (x);
+	else if (s[x] == 43)
+	{
+		x++;
+	}
+	while (s[x] >= 48 && s[x] <= 57)
+	{
+		result = (result * 10) + (s[x] - '0');
+		x++;
+	}
+	return (sign * result);
 }
-
