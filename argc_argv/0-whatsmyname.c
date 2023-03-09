cfd5d9ee1bb77053cb146febfc6914b81182924a
Aldo2303 <aldod03@gmail.com>
2023-03-09 08:52:42 -0800

    arg0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* main - program that prints its name
+* @argc: variable
+* @argv: variable
+* Return: int
+*/
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+	printf("%s\n", argv[0]);
+	}
+	return (0);
+}
