0c815d99be845ed15c8d77341a2562b64214a5c6
Aldo2303 <aldod03@gmail.com>
2023-03-10 04:35:55 -0800

    arg3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - program that prints its name
+ * @argc: variable
+ * @argv: variable
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+		return (0);
+}
