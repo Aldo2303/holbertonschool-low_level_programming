2f424c20cf935f3dd2e2c47674b0bea5140b4425
Aldo2303 <aldod03@gmail.com>
2023-03-14 08:49:52 -0700

    malloc3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,22 +10,27 @@
 int **alloc_grid(int width, int height)
 {
 	int **ptr_matriz;
-	int x, y;
+	int x, z, a;
 
 	if (width <= 0)
 		return (NULL);
 	if (height <= 0)
 		return (NULL);
-	ptr_matriz = (int**)malloc(sizeof(int) * height);
-	if (!ptr_matriz)
+	ptr_matriz = (int **)malloc(sizeof(int) * height);
+	if (ptr_matriz == NULL)
 		return (NULL);
 	for (x = 0; x < height; x++)
 	{
-		ptr_matriz[x] = (int*)malloc(sizeof(int) * width);
-		if (!ptr_matriz)
+		ptr_matriz[x] = (int *)malloc(sizeof(int) * width);
+		if (ptr_matriz[x] == NULL)
 			return (NULL);
-		for (y = 0; y < width; y++)
-			ptr_matriz[x][y] = 0;
 	}
-	return (ptr_matriz);
+		for (z = 0; z < height; z++)
+		{
+			for (a = 0; a < width; a++)
+			{
+				ptr_matriz[z][a] = 0;
+			}
+		}
+		return (ptr_matriz);
 }
