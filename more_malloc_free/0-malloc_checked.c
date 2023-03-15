d018115e8360620dee2ff5f42d05131c257847b5
Aldo2303 <aldod03@gmail.com>
2023-03-15 16:49:31 -0700

    Mmalloc0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -4,13 +4,14 @@
 /**
 * malloc_checked - function that returns a pointer to a 2 dimensional array
 * @b: the variable
+* Return: void
 */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
-	ptr = malloc(sizeof(int) * b);
+	ptr = malloc(b);
 	if (ptr == NULL)
-		exit (98);
+		exit(98);
 	return (ptr);
 }
