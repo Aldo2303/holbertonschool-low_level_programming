f9a7a8db75fa13df2f286e91d46b38fd6750adaa
Aldo2303 <aldod03@gmail.com>
2023-03-20 06:35:51 -0700

    task1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "dog.h"
 /**
- * init dog - function that initialize a variable of type struct dog
+ * init_dog - function that initialize a variable of type struct dog
  * @d: variable
  * @name: variable
  * @owner: variable
