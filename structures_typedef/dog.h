d12b9cb93d6c0a86d4b5b8823503c75205466423
Aldo2303 <aldod03@gmail.com>
2023-03-20 06:17:34 -0700

    task0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - Define a new type struct dog
+ * @name: the variable
+ * @owner: the variable
+ * @age: the variable
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+
+#endif
+
