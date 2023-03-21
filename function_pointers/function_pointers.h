323be65416293640821006b3fe55d6dd48801fe6
Aldo2303 <aldod03@gmail.com>
2023-03-21 04:50:36 -0700

    fun.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,10 @@
+#ifndef FUN_POINTER
+#define FUN_POINTER
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+
+#endif
