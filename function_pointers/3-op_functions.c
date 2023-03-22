5d678280066db83fd008aeec44eaafb49b6d06d6
Aldo2303 <aldod03@gmail.com>
2023-03-22 04:08:33 -0700

    task3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+ * op_add - operator addition
+ * @a: variable
+ * @b: variable
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - operator subtraction
+ * @a: variable
+ * @b: variable
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - operator multiplication
+ * @a: variable
+ * @b: variable
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - operator division
+ * @a: variable
+ * @b: variable
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - operator modulo
+ * @a: variable
+ * @b: variable
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
