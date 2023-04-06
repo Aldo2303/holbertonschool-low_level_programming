5b6a8fd5cde55f830ffe7d85b10e6c3bdccd35d7
Aldo2303 <aldod03@gmail.com>
2023-04-06 08:10:27 -0700

    task1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - function returns the number of elements list
+ * @h: variable
+ * Return: size_t
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
