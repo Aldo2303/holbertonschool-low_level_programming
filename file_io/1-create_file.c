d34a3852aea0be8b42031646404765180cef6192
Aldo2303 <aldod03@gmail.com>
2023-04-11 09:12:43 -0700

    task1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
 
 	/*write the textContent and len of string*/
