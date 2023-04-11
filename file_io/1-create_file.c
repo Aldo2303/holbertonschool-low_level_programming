6148d505365c272288b90e4652ab38910ce0dbb9
Aldo2303 <aldod03@gmail.com>
2023-04-11 09:10:18 -0700

    task1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * create_file - Create a function that creates a file
+ * @filename: file where to read
+ * @text_content: pointer with content to string
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, chars_write;
+
+	if (filename == NULL)
+		return (-1);
+
+	/*man open, permission when create fd 00700 has read, write and exec*/
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 00600);
+	if (fd == -1)
+		return (-1);
+
+	/*if textContent is NULL I need create an empty file*/
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	/*write the textContent and len of string*/
+	chars_write = write(fd, text_content, strlen(text_content));
+	if (chars_write == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
