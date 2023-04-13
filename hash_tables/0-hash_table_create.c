55a49dcd489a7e4290b293f79f9b9e52de8b4013
Aldo2303 <aldod03@gmail.com>
2023-04-13 06:14:58 -0700

    task0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	newTable->size = size;
-	newTable->array = malloc(sizeof(hash_node_t) * size);
+	newTable->array = malloc(sizeof(hash_node_t*) * size);
 	if (newTable->array == NULL)
 	{
 		free(newTable);
